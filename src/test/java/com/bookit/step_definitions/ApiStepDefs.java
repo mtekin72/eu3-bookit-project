fb5d6ae8dc127e9e3ed0adf26710f648fd97fc46
CybertekSchool <github@cybertekschool.com>
2020-12-03 11:53:38 -0500

    git ignore

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,18 +1,5 @@
 package com.bookit.step_definitions;
 
-import com.bookit.pages.SelfPage;
-import com.bookit.utilities.BookItApiUtils;
-import com.bookit.utilities.ConfigurationReader;
-import com.bookit.utilities.DBUtils;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.junit.Assert;
-
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
 
 public class ApiStepDefs {
